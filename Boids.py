2f764370b7e6b3fb0b11438a700e1a91c0c17031
Terry01001 <98895107+Terry01001@users.noreply.github.com>
2023-10-23 23:13:10 +0800

    first commit

---
+++ Boids.py
@@ -0,0 +1,176 @@
+import pygame as pg
+import random
+import math
+
+class Boid(pg.sprite.Sprite):
+    def __init__(self, loc_init):
+        super().__init__()
+        self.image = pg.Surface((10, 10))
+        self.color = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
+        self.image.fill(self.color)
+        self.rect = self.image.get_rect()
+
+        #initialize speed,heading location
+        # Inside the Boid class, initialize speed with some randomness
+        self.speed = random.uniform(3, 5)  # Adjust the range as needed
+        self.heading = pg.Vector2(1,1)
+        self.loc = loc_init
+        self.rect.x = self.loc[0]
+        self.rect.y = self.loc[1]   
+
+    def update(self, boids):
+        # 分别计算三个规则的调整向量
+        separation_vector = self.calculate_separation(boids)
+        alignment_vector = self.calculate_alignment(boids)
+        cohesion_vector = self.calculate_cohesion(boids)
+
+        # 调整速度和方向
+        self.heading = self.heading + separation_vector + alignment_vector + cohesion_vector
+
+        # 限制速度的最大值
+        max_speed = 5
+        if self.heading.length() > max_speed:
+            self.heading = self.heading.normalize() * max_speed
+
+        # 根据新的方向更新位置
+        self.rect.x += self.heading.x
+        self.rect.y += self.heading.y
+
+        # 边界处理，让鳥類在窗口内移動
+        if self.rect.left > width:
+            self.rect.right = 0
+        elif self.rect.right < 0:
+            self.rect.left = width
+        if self.rect.top > height:
+            self.rect.bottom = 0
+        elif self.rect.bottom < 0:
+            self.rect.top = height
+
+
+    def calculate_separation(self, boids):
+        # 计算分离向量
+        separation_radius = 50
+        separation_vector = pg.Vector2(0, 0)
+        
+        for boid in boids:
+            if boid != self:
+                distance = pg.Vector2(boid.rect.center) - pg.Vector2(self.rect.center)
+                if 0 < distance.length() < separation_radius:
+                    separation_vector -= distance.normalize()
+
+        return separation_vector
+
+    def calculate_alignment(self, boids):
+        # 计算对齐向量
+        alignment_radius = 100
+        alignment_vector = pg.Vector2(0, 0)
+        count = 0
+
+        for boid in boids:
+            #if boid != self:
+                # 使用math.atan2()计算弧度值
+            angle_rad = math.atan2(boid.heading.y, boid.heading.x)
+            angle_deg = math.degrees(angle_rad)
+            distance = pg.Vector2(boid.rect.center) - pg.Vector2(self.rect.center)
+            if 0 < distance.length() < alignment_radius:
+                alignment_vector += pg.Vector2(math.cos(math.radians(angle_deg)), math.sin(math.radians(angle_deg)))
+                count += 1
+
+        if count > 0:
+            alignment_vector /= count
+
+        return alignment_vector
+
+
+    def calculate_cohesion(self, boids):
+        # 计算聚集向量
+        cohesion_radius = 150
+        cohesion_center = pg.Vector2(0, 0)
+        count = 0
+
+        for boid in boids:
+            if boid != self:
+                distance = pg.Vector2(boid.rect.center) - pg.Vector2(self.rect.center)
+                if 0 < distance.length() < cohesion_radius:
+                    cohesion_center += pg.Vector2(boid.rect.center)
+                    count += 1
+
+        if count > 0:
+            cohesion_center /= count
+            cohesion_vector = cohesion_center - pg.Vector2(self.rect.center)
+            epsilon = 1e-5
+            if cohesion_vector.length() > epsilon:
+                return cohesion_vector.normalize()
+            else:
+                return pg.Vector2(0, 0)
+
+        else:
+            return pg.Vector2(0, 0)
+        
+
+
+# # 初始化Pygame
+# pg.init()
+
+# # 設置窗口尺寸和顯示
+# width, height = 800, 600
+# screen = pg.display.set_mode((width, height))
+# pg.display.set_caption("Boids Simulation")
+
+
+# Initialize Pygame
+pg.init()
+
+# Get the current screen display mode
+screen_info = pg.display.Info()
+width, height = screen_info.current_w, screen_info.current_h
+
+# Set the display mode to fullscreen
+fullscreen = True
+screen = pg.display.set_mode((width, height), pg.FULLSCREEN)
+pg.display.set_caption("Boids Simulation")
+
+
+
+# 創建一個鳥類群集
+boids_group = pg.sprite.Group()
+for i in range(100):
+    boids_group.add(Boid([random.randint(0, width), random.randint(0, height)]))
+
+
+clock = pg.time.Clock()
+
+# 主循環
+running = True
+while running:
+    clock.tick(60)
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+        elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
+            if fullscreen:
+                # Switch back to windowed mode
+                screen = pg.display.set_mode((width, height))
+            running = False  # Exit the program
+
+    # 清空畫面
+    screen.fill((0,0,0))
+    
+
+    # 移動和繪製每個鳥類
+    boids_group.update(boids_group)
+
+    boids_group.draw(screen)
+    #pg.display.flip()
+
+    # 更新畫面
+    pg.display.flip()
+
+# 關閉Pygame
+pg.quit()
+
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,29 @@
+# Boids Simulation
+
+## Overview
+Boids is a simulation of flocking behavior, which mimics the collective movement of birds, fish, or other animals. In this project, we implement the key principles of Boids: Separation, Alignment, and Cohesion.
+
+## Project Description
+This project is part of my learning journey in computer science, where I'm exploring AI concepts and practical coding. Boids provides a fascinating way to understand emergent behavior in a group of simple agents.
+
+## Boid Behaviors
+The Boid simulation consists of three main behaviors:
+
+### Separation
+Separation ensures that Boids avoid colliding with each other. Each Boid steers away from its nearby neighbors, preventing collisions and maintaining personal space.
+
+### Alignment
+Alignment focuses on Boids aligning their velocities with those of their neighbors. This behavior simulates the tendency of animals to match the direction and speed of their group.
+
+### Cohesion
+Cohesion encourages Boids to move towards the center of mass of their neighboring Boids. It creates a cohesive force that keeps the group together.
+
+## Usage
+To run the Boids simulation, follow these steps:
+
+1. Clone the repository to your local machine.
+2. Open the project in your preferred programming environment.
+3. Execute the main simulation script.
+
+```python
+python boids_simulation.py
